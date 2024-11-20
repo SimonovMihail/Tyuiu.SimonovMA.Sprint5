06291503366f06740fb53cddf4a8042bf6545f6c
Tyuiu.SimonovMA@outlook.com <Tyuiu.SimonovMA@outlook.com>
2024-11-20 20:43:26 +0500

    Task5 Definetily Fixed

---
+++ xd/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace xd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Path.Combine("C:", "DataSprint5", "InPutDataFileTask5V10.txt");
+            string[] numbers = File.ReadAllText(path).Split('\n');
+            double sum = 0;
+            foreach (string number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
