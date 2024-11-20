c85b37f7e97e5e4e975a3d4d3f8d19a4e5f08421
Tyuiu.SimonovMA@outlook.com <Tyuiu.SimonovMA@outlook.com>
2024-11-20 11:14:40 +0500

    Task4 Fixed

--- Tyuiu.SimonovMA.Sprint5.Task4.V9.Lib/DataService.cs
+++ Tyuiu.SimonovMA.Sprint5.Task4.V9.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.SimonovMA.Sprint5.Task4.V9.Lib
     {
         public double LoadFromDataFile(string path)
         {
-            string strX = File.ReadAllText(path);
+            string strX = File.ReadAllText(path).Replace('.', ',');
             double res = 1.0 / Math.Sin(Convert.ToDouble(strX)) + Math.Pow(Convert.ToDouble(strX), 2);
             return Math.Round(res, 3);
         }
